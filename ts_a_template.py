050514ab6aca1729588851d439c9790a25c7d206
zbingf <zbingf@126.com>
2021-04-14 21:22:01 +0800

    updata

---
+++ ts_a_template.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+def glyphscript(engineState):
+	ts_in0 = engineState.GetInputTimeSeries(0)
+	ts_out0 = engineState.GetOutputTimeSeries(0)
+	list1 = getTS(ts_in0)
+	putTS(ts_out0, ts_in0, list1)
+	
+	return ''
+
+def putTS(tsobj,tartsobj,list1):
+	# 对时间序列进行赋值
+	# 复制tartsobj属性 到 tsobj中
+	# 将列表list1 作为数据 导入 tsobj中
+	import array
+	num = len(list1)
+	mdobj = tsobj.GetMetaData()
+	tarmdobj = tartsobj.GetMetaData()
+
+	if type(list1[0]) == list :
+		tsobj.SetChannelCount(num)
+		len_value = len(list1[0])
+		for n in range(num):
+			tsobj.CopyAttributes(tartsobj, n, n)
+			tsobj.CopyMetaData(tartsobj, n, n)
+			tsobj.SetPointCount(n, len_value)
+			arr1 = array.array('f', list1[n])
+			tsobj.PutValues(n, 0, len_value,arr1)
+	else:
+		tsobj.SetChannelCount(1)
+		tsobj.CopyAttributes(tartsobj, 0, 0)
+		tsobj.CopyMetaData(tartsobj, 0, 0)
+		tsobj.PutValues(0, num, 1, list1)
+
+	name = tarmdobj.GetItem(-1, 'TestName')
+	mdobj.SetItem(-1, 'InputTestInfo', 'TestName', 'string', name)
+
+	return None
+
+def getTS(tsobj):
+	# 获取 time series 数据
+	# 转化为列表导出
+	num = tsobj.GetChannelCount()
+	list1 = []
+	for n in range(num):
+		listnum = tsobj.GetPointCount(n)
+		list_temp = tsobj.GetValuesAsList(n,0,listnum)
+		list1.append(list_temp)
+	return list1
--- ts_fun.py
+++ ts_fun.py
@@ -16,26 +16,39 @@ def ts_extend(tartsobj, tsobj1, tsobj2):
 	# 数据拼接
 	putTS(tartsobj,tsobj1,list1)
 
-#获取\赋值时序信号数据
+	return None
 
+
+#获取\赋值时序信号数据
 def putTS(tsobj,tartsobj,list1):
 	# 对时间序列进行赋值
 	# 复制tartsobj属性 到 tsobj中
 	# 将列表list1 作为数据 导入 tsobj中
 	import array
 	num = len(list1)
+	mdobj = tsobj.GetMetaData()
+	tarmdobj = tartsobj.GetMetaData()
+
 	if type(list1[0]) == list :
 		tsobj.SetChannelCount(num)
 		len_value = len(list1[0])
 		for n in range(num):
-			tsobj.CopyAttributes(tartsobj,n,n)
-			tsobj.SetPointCount(n,len_value)
-			arr1 = array.array('f',list1[n])
-			tsobj.PutValues(n,0,len_value,arr1)
+			tsobj.CopyAttributes(tartsobj, n, n)
+			tsobj.CopyMetaData(tartsobj, n, n)
+			tsobj.SetPointCount(n, len_value)
+			arr1 = array.array('f', list1[n])
+			tsobj.PutValues(n, 0, len_value,arr1)
 	else:
 		tsobj.SetChannelCount(1)
-		tsobj.CopyAttributes(tartsobj,0,0)
-		tsobj.PutValues(0,num,1,list1)
+		tsobj.CopyAttributes(tartsobj, 0, 0)
+		tsobj.CopyMetaData(tartsobj, 0, 0)
+		tsobj.PutValues(0, num, 1, list1)
+
+	name = tarmdobj.GetItem(-1, 'TestName')
+	mdobj.SetItem(-1, 'InputTestInfo', 'TestName', 'string', name)
+
+	return None
+
 
 def getTS(tsobj):
 	# 获取 time series 数据
@@ -46,6 +59,7 @@ def getTS(tsobj):
 		listnum = tsobj.GetPointCount(n)
 		list_temp = tsobj.GetValuesAsList(n,0,listnum)
 		list1.append(list_temp)
+		
 	return list1
 
 def cal_rms_delta_percent(list1,list2):
